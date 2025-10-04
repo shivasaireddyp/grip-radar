515cfef44e92f83dbb97e8b63b96da48abeaa4f1
shivasaireddy <shivasaireddyp@gmail.com>
2025-10-05 00:08:57 +0530

    made some ui changes in app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.set_page_config(page_title="Customer Retention Prediction", page_icon="🏦")
+st.set_page_config(page_title="Grip Radar", page_icon="🏦")
 
 st.title("Grip Radar: A Customer Retention Prediction App")
 
 st.markdown("""
 ### Hi, Welcome!
 
-This app helps predict banks whether a customer is likely to **stay** or **leave** them.
+Grip Radar helps predict banks whether a customer is likely to stay **gripped** or **leave** them.
 
 Please use the sidebar to navigate between modes.
 
